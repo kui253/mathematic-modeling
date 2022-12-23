e39d14232e26fc6c92ba1b5e084e6863428d8c8e
whd <2533917115@qq.com>
2022-12-23 22:24:43 +0800

    爬虫资料更新到requests模块的初次demo

---
+++ WebSpider/requests_test.py
@@ -0,0 +1,17 @@
+#requests库的第一次test使用
+#testor ：whd
+#需求：爬取搜狗网页的数据
+import requests
+if __name__ == "__main__":
+    #step1:指定Url
+    url = "https://www.sogou.com/"
+    #step2：发起请求
+    response = requests.get(url=url)#返回一个响应对象
+    #step3:获取响应数据
+    page_text = response.text#字符串形式数据
+    print(page_text)
+    #step4:存储
+    with open('./data/sogou.html','w',encoding='utf-8') as fp:
+        fp.write(page_text)
+    print('数据爬取结束')
+
